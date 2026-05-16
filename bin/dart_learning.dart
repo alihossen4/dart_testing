0cb793ff99b468ef72f134cad1e57cbf44f45116
alihossen4 <mdahossen.dev@gmail.com>
2026-05-16 14:24:22 +0600

    feat: changes on dart_larning

--- bin/dart_learning.dart
+++ bin/dart_learning.dart
@@ -2,8 +2,7 @@ void main() {
   String? name = "ali";
   String? greeting = "Hello";
   print("$greeting ${name ?? "Guest"}");
-  String? prof;
-  print(prof!.length);
+
   List<int> nums = [1, 2, 3]
     // nums.add(4);
     ..add(5)
